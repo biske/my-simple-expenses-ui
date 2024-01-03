a50e5216a3ae4355d0bb5bdb317c5920f95dd923
Ivan Bisevac <ivan.bisevac@gmail.com>
2024-01-03 23:52:13 +0100

    Update the layout of the app using Tailwind classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="p-4 text-center text-blue-500">My Simple Expenses</div>
+    <div class="app h-screen w-full">
+      <header class="bg-blue-500 text-white text-lg p-4 fixed top-0 w-full">
+        My Simple Expenses
       </header>
+      <div class="main-content pt-16 p-4">
+        <button class="bg-green-500 text-white p-2 rounded hover:bg-green-600 transition duration-300">
+          Add Expense
+        </button>
+      </div>
     </div>
   );
 }
