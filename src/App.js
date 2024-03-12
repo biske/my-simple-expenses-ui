08fe054b59433759895a70063826cc6567cbb0a1
Ivan Bisevac <ivan.bisevac@gmail.com>
2024-03-13 02:48:04 +0300

    Add @tanstack/react-query to the project and Radnom component as a placeholder to demonstrate the feature (should be removed once adding simple expense is wired with the react query)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { useForm } from 'react-hook-form';
+import { Random } from './Random';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 import "./App.css";
+
+const queryClient = new QueryClient()
 
 function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -10,40 +17,43 @@ function App() {
   };
 
   return (
-    <div className="app h-screen w-full">
-      <header className="bg-blue-500 text-white text-lg p-4 fixed top-0 w-full">
-        My Simple Expenses
-      </header>
-      <div className="main-content pt-16 p-4">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div>
-            <label htmlFor="amount" className="block text-sm font-medium text-gray-700">Amount</label>
-            <input
-              id="amount"
-              {...register('amount', { required: true })}
-              type="number"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-            {errors.amount && <span className="text-red-500 text-xs">This field is required</span>}
-          </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app w-full">
+        <header className="bg-blue-500 text-white text-lg p-4 fixed top-0 w-full">
+          My Simple Expenses
+        </header>
+        <div className="main-content pt-16 p-4">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <label htmlFor="amount" className="block text-sm font-medium text-gray-700">Amount</label>
+              <input
+                id="amount"
+                {...register('amount', { required: true })}
+                type="number"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              {errors.amount && <span className="text-red-500 text-xs">This field is required</span>}
+            </div>
 
-          <div>
-            <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description</label>
-            <textarea
-              id="description"
-              {...register('description', { required: true })}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              rows="3"
-            ></textarea>
-            {errors.description && <span className="text-red-500 text-xs">This field is required</span>}
-          </div>
+            <div>
+              <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description</label>
+              <textarea
+                id="description"
+                {...register('description', { required: true })}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                rows="3"
+              ></textarea>
+              {errors.description && <span className="text-red-500 text-xs">This field is required</span>}
+            </div>
 
-          <button type="submit" className="bg-green-500 text-white p-2 rounded hover:bg-green-600 transition duration-300">
-            Add Expense
-          </button>
-        </form>
+            <button type="submit" className="bg-green-500 text-white p-2 rounded hover:bg-green-600 transition duration-300">
+              Add Expense
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+      <Random />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/Random.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchNumber = () => {
+  return fetch("https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new")
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error("Something went wrong. Try again.")
+      }
+
+      return response.text();
+    })
+}
+
+export const Random = () => {
+  const query = useQuery({ queryKey: ['random'], queryFn: fetchNumber })
+
+  if (query.isError) return <p>{query.error.message}</p>
+
+  return (
+    <button onClick={() => query.refetch()}>
+      Random number: {
+        query.isLoading || query.isFetching ? "..." : query.data
+      }
+    </button>
+  );
+};
